e73cd3c4f525f72cc95c515aa5ecdfbb17270eb9
Delaney H <11yomang11@gmail.com>
2023-07-14 19:06:12 -0230

    Basic project folder creation

---
+++ webserver.js
@@ -0,0 +1,5 @@
+//TODO: create homepage
+
+//TODO: create a page that displays a list of images based on a search request
+
+//TODO: create a page that displays one image, with its related tags, and any comments attached to it
