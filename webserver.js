a0789da0cfabd5467599d955484b8023d4b593fc
Ben <Benjamin.Stapleton89@ed.cna.nl.ca>
2023-07-17 17:59:13 -0230

    Trying to get comments to post

--- webserver.js
+++ webserver.js
@@ -1,31 +1,74 @@
-//TODO: create homepage
 
-//TODO: create a page that displays a list of images based on a search request
-
-//TODO: create a page that displays one image, with its related tags, and any comments attached to it
-
-//TODO: implement "promises"
 
 const http = require('http');
-const url = require("url");
-const fs = require('fs');
-
+const fs = require('fs').promises;
+const url = require('url');
 /*
-BELOW:
-    - Basic creation of server
-    - Has homepage & 2 buttons; allows user to move to two pages
-RESOURCE USED --- https://www.w3schools.com/nodejs/nodejs_url.asp
+NOTE:
+    Used prof notes as reference for stuff below
  */
-http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    const file_name = "." + q.pathname;
-    fs.readFile(file_name, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+
+const getFile = (res, path, content_type) => {
+    res.writeHead(200, {'Content-Type': content_type});
+    fs.readFile(path)
+        .then(content => res.write(content))
+        .then(_ => res.end());
+}
+
+http.createServer((req, res) => {
+    const p = req.url.split("/");
+    console.log(req.url + "\trequest url");
+    console.log(p[1] + "\tending of file");
+    if (p[1] === "" || p[1] === "homepage.html") {
+        getFile(res, "homepage.html", "text/html");
+    }
+    else if (p[1] === "image_list.html" || p[1] === "view_image.html") {
+        getFile(res, p[1], "text/html");
+    }
+    else if (p[1] === "homepage.css" || p[1] === "image_list.css"|| p[1] === "view_image.css") {
+        getFile(res, p[1], "text/css");
+    }
+    else if (p[1] === "script.js" || p[1] === "view_image.js") {
+        getFile(res, p[1], "text/javascript");
+    }
+    /*
+    else if (p[3].endsWith(".png")) {
+        res.writeHead(200, {'Content Type': 'image/png'});
+        res.end(p[3], 'binary')
+    }
+     */
+    else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write("<html><head></head><body><p>Oops! No content here.</p></body><html/>");
+        res.end();
+    }
+
+    if (req.method === "POST" && p[1] === "view_image") {
+        let body = "";
+
+        req.on("data", (chunk) => {
+            body += chunk;
+        });
+
+        req.on("end", () => {
+            // Parse the JSON data from the request body
+            try {
+                const postData = JSON.parse(body);
+                // Handle the contents here, e.g., store it in a database or do other processing
+                console.log(postData.contents);
+
+                // Respond with a success status and message
+                res.writeHead(200, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({status: "success", message: "Comment posted successfully"}));
+            } catch (error) {
+                // Handle any errors while parsing JSON or processing data
+                console.error("Error occurred while processing POST request:", error);
+
+                // Respond with an error status and message
+                res.writeHead(500, {"Content-Type": "application/json"});
+                res.end(JSON.stringify({status: "error", message: "Internal server error"}));
+            }
+        })
+    }
 }).listen(8080);
+
