a41d1957df13530edca4d79396e0283d01ffe4d5
Delaney H <11yomang11@gmail.com>
2023-07-17 11:10:48 -0230

    Added script.js (for programming), comments.json and images.json (for savings image links and comments associated with images)

---
+++ script.js
@@ -0,0 +1,9 @@
+//TODO: create homepage
+
+//TODO: create a page that displays a list of images based on a search request
+
+//TODO: create a page that displays one image, with its related tags, and any comments attached to it
+
+//TODO: implement "promises"
+
+// NOTE: THIS IS FOR JAVASCRIPT CODE, THE OTHER FILE IS FOR INITIATING THE WEBSERVER
--- webserver.js
+++ webserver.js
@@ -1,11 +1,3 @@
-//TODO: create homepage
-
-//TODO: create a page that displays a list of images based on a search request
-
-//TODO: create a page that displays one image, with its related tags, and any comments attached to it
-
-//TODO: implement "promises"
-
 const http = require('http');
 const url = require("url");
 const fs = require('fs');
