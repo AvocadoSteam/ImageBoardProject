d6eba68cd94a9a20f89aa2f29929c7bc120c4a96
Delaney H <11yomang11@gmail.com>
2023-07-17 15:20:31 -0230

    Moved images.json. Restructed json file. Created comments.json which links with an id from images.json

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 //TODO: create a page that displays a list of images based on a search request
 
-//TODO: create a page that displays one image, with its related tags, and any comments attached to it
+//TODO: create a page that displays one image, with its related tags, and any comments.json attached to it
 
 //TODO: implement "promises"
 
