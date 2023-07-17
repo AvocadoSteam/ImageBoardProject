47cdb77ba650051330f90a92128c10a168f04331
Delaney H <11yomang11@gmail.com>
2023-07-17 14:21:13 -0230

    Delete unnecesary node module. CSS now displays properly (but images don't)

--- webserver.js
+++ webserver.js
@@ -1,29 +1,37 @@
 const http = require('http');
-const url = require("url");
-const fs = require('fs');
-const express = require('express');
-const app = express();
-const path = require('path');
+const fs = require('fs').promises;
 
-// https://stackoverflow.com/questions/50441000/include-css-and-js-files-in-node-js
-app.use(express.static(__dirname + '/public'));
 /*
-BELOW:
-    - Basic creation of server
-    - Has homepage & 2 buttons; allows user to move to two pages
-RESOURCE USED --- https://www.w3schools.com/nodejs/nodejs_url.asp
+NOTE:
+    Used prof notes as reference for stuff below
  */
-http.createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    const file_name = "." + q.pathname;
-    fs.readFile(file_name, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            return res.end("404 Not Found");
-        }
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+const getFile = (res, path, content_type) => {
+    res.writeHead(200, {'Content-Type': content_type});
+    fs.readFile(path)
+        .then(content => res.write(content))
+        .then(_ => res.end());
+}
+
+http.createServer((req, res) => {
+    const p = req.url.split("/");
+    console.log(req.url);
+    console.log(p[1]);
+    if (p[1] === "" || p[1] === "homepage.html") {
+        getFile(res, "homepage.html", "text/html");
+    }
+    else if (p[1] === "image_list.html" || p[1] === "view_image.html") {
+        getFile(res, p[1], "text/html");
+    }
+    else if (p[1] === "homepage.css" || p[1] === "image_list.css"|| p[1] === "view_image.css") {
+        getFile(res, p[1], "text/css");
+    }
+    else if (p[1] === "script.js") {
+        getFile(res, p[1], "text/javascript");
+    }
+    else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.write("<html><head></head><body><p>Oops! No content here.</p></body><html/>");
+        res.end();
+    }
 }).listen(8080);
 
