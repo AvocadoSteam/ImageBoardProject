507e41a9dc26d26bdf5309008a09e4151e57764d
Delaney H <11yomang11@gmail.com>
2023-07-20 17:21:03 -0230

    Added tags as cookie

--- homepage.js
+++ homepage.js
@@ -6,5 +6,11 @@ $(document).ready(async () => {
         const image_id = $("#image_id_text").val();
         document.cookie = `image_id=${image_id}; max-age=7200; path=/`; // establishes the image that should be loaded
         location.replace('view_image.html');
+    });
+    $("#lookup-button").click( async () => {
+        const tags = $("#topic-id-selection").val();
+        const tagsAsCookie = JSON.stringify(tags.split(","));
+        document.cookie = `tags=${tagsAsCookie}; max-age=7200; path=/`; // establishes the image that should be loaded
+        location.replace('image_list.html');
     })
 });
